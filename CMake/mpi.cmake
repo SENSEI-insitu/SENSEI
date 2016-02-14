c6d03b0446cebd67c6cfd703bef78bafedbd0a11
Utkarsh Ayachit <utkarsh.ayachit@kitware.com>
2016-02-14 15:19:49 -0500

    CMake cleanups.
    Cleaning up MPI linking by adding a new interface library for MPI.

--- CMake/mpi.cmake
+++ CMake/mpi.cmake
@@ -1,15 +1,14 @@
 #------------------------------------------------------------------------------
 # Since we need MPI left and right, this makes it easier to deal with MPI.
 find_package(MPI REQUIRED)
-function(mpi_link target)
-  target_include_directories(${target}
-    SYSTEM PRIVATE ${MPI_C_INCLUDE_PATH} ${MPI_CXX_INCLUDE_PATH})
-  target_compile_definitions(${target}
-    PRIVATE ${MPI_C_COMPILE_FLAGS} ${MPI_CXX_COMPILE_FLAGS})
-  target_link_libraries(${target}
-    PRIVATE ${MPI_C_LIBRARIES} ${MPI_CXX_LIBRARIES})
-  if(MPI_C_LINK_FLAGS OR MPI_CXX_LINK_FLAGS)
-    set_target_properties(${target}
-      PROPERTIES LINK_FLAGS ${MPI_C_LINK_FLAGS} ${MPI_CXX_LINK_FLAGS})
-  endif()
-endfunction()
+add_library(mpi INTERFACE)
+target_include_directories(mpi
+  SYSTEM INTERFACE ${MPI_C_INCLUDE_PATH} ${MPI_CXX_INCLUDE_PATH})
+target_compile_definitions(mpi
+  INTERFACE ${MPI_C_COMPILE_FLAGS} ${MPI_CXX_COMPILE_FLAGS})
+target_link_libraries(mpi
+  INTERFACE ${MPI_C_LIBRARIES} ${MPI_CXX_LIBRARIES})
+if(MPI_C_LINK_FLAGS OR MPI_CXX_LINK_FLAGS)
+  set_target_properties(mpi
+    PROPERTIES LINK_FLAGS ${MPI_C_LINK_FLAGS} ${MPI_CXX_LINK_FLAGS})
+endif()
