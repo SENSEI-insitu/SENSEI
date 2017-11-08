a47f0f9b5f554a15fdb2b8a8501e89d610f5cfb7
Burlen Loring <bloring@lbl.gov>
2017-11-08 12:50:15 -0800

    CTest support
    * Add support for CTest and configure dashboard at
      cdash.hpcvis.com
    
    * sensei_add_test CMake function.  This function handles a number of
      common tasks associated with adding regression tests, such as
      compiling test executable, enabling tests based on the projects CMake
      options, and parsing test output for occurances of strings like
      "ERROR" that indicate a failure.

---
+++ CMake/testing.cmake
@@ -0,0 +1,49 @@
+set(SENSEI_DATA_ROOT "" CACHE PATH "Path to SENSEI test data")
+set(BUILD_TESTING OFF CACHE BOOL "Enable tests")
+if (BUILD_TESTING)
+  enable_testing()
+  include(CTest)
+endif()
+
+# sensei_add_test(name
+#   EXEC_NAME -- optional, name of the copiled test
+#   SOURCES  -- optional, source files to comile
+#   LIBS -- optional, libraries to link to the compiled test
+#   COMMAND -- required, test command
+#   FEATURES -- optional, boolean condition decribing feature dependencies
+#   REQ_SENSEI_DATA -- flag whose presence indicates the test needs the data repo
+#   )
+function (sensei_add_test T_NAME)
+  set(opt_args REQ_SENSEI_DATA)
+  set(val_args EXEC_NAME)
+  set(array_args SOURCES LIBS COMMAND FEATURES)
+  cmake_parse_arguments(T "${opt_args}" "${val_args}" "${array_args}" ${ARGN})
+  set(TEST_ENABLED ON)
+  if (NOT DEFINED T_FEATURES)
+    set(TEST_ENABLED ON)
+  else()
+    foreach(feature ${T_FEATURES})
+      if (NOT feature)
+        set(TEST_ENABLED OFF)
+      endif()
+    endforeach()
+  endif()
+  if (TEST_ENABLED)
+    if (T_SOURCES)
+      set(EXEC_NAME ${T_NAME})
+      if (T_EXEC_NAME)
+        set(EXEC_NAME ${T_EXEC_NAME})
+      endif()
+      add_executable(${EXEC_NAME} ${T_SOURCES})
+      if (T_LIBS)
+        target_link_libraries(${EXEC_NAME} ${T_LIBS})
+      endif()
+    endif()
+    if ((T_REQ_SENSEI_DATA AND SENSEI_DATA_ROOT) OR NOT T_REQ_SENSEI_DATA)
+      add_test(NAME ${T_NAME} COMMAND ${T_COMMAND}
+        WORKING_DIRECTORY ${CMAKE_RUNTIME_OUTPUT_DIRECTORY})
+      set_tests_properties(${T_NAME}
+        PROPERTIES FAIL_REGULAR_EXPRESSION "[Ee][Rr][Rr][Oo][Rr]")
+    endif()
+  endif()
+endfunction()
