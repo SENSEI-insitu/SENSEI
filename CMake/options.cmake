59a55526a269a789fbe9566d3a1c83dee7777dca
Brad Whitlock <bjw.ilight@gmail.com>
2018-04-17 13:45:03 -0700

    First working version of mandelbrot with VTK AMR dataset. Libsim can plot the VTK dataset as AMR.

--- CMake/options.cmake
+++ CMake/options.cmake
@@ -35,6 +35,7 @@ cmake_dependent_option(ENABLE_VTK_M
 
 option(ENABLE_PARALLEL3D "Enable Parallel3D miniapp" ON)
 option(ENABLE_OSCILLATORS "Enable Oscillators miniapp" ON)
+option(ENABLE_MANDELBROT "Enable Mandelbrot miniapp" ON)
 
 message(STATUS "ENABLE_SENSEI=${ENABLE_SENSEI}")
 message(STATUS "ENABLE_PYTHON=${ENABLE_PYTHON}")
