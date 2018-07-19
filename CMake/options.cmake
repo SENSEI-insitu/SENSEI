543b57ff96a01eeed6e037f4b0b99f4074c3baee
Brad Whitlock <bjw.ilight@gmail.com>
2018-07-18 17:47:46 -0700

    Added vortex miniapp to build.

--- CMake/options.cmake
+++ CMake/options.cmake
@@ -36,6 +36,7 @@ cmake_dependent_option(ENABLE_VTK_M
 option(ENABLE_PARALLEL3D "Enable Parallel3D miniapp" ON)
 option(ENABLE_OSCILLATORS "Enable Oscillators miniapp" ON)
 option(ENABLE_MANDELBROT "Enable Mandelbrot miniapp" ON)
+option(ENABLE_VORTEX "Enable Vortex miniapp" ON)
 
 message(STATUS "ENABLE_SENSEI=${ENABLE_SENSEI}")
 message(STATUS "ENABLE_PYTHON=${ENABLE_PYTHON}")
