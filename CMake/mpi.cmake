54dd88223a74355a4c2ddce8d25064f95ab2c64b
Burlen Loring <bloring@lbl.gov>
2018-09-12 10:58:23 -0700

    more fixes for CMake 3.11.2 FindMPI

--- CMake/mpi.cmake
+++ CMake/mpi.cmake
@@ -1,10 +1,14 @@
-find_package(MPI REQUIRED)
+find_package(MPI)
+if (NOT MPI_C_FOUND)
+  message(FETAL_ERROR "Failed to locate MPI C libraries and headers")
+endif()
+
 add_library(mpi INTERFACE)
 
-target_include_directories(mpi SYSTEM INTERFACE ${MPI_C_INCLUDE_PATH}
-  ${MPI_CXX_INCLUDE_PATH} ${MPI_C_INCLUDE_DIRS}  ${MPI_CXX_INCLUDE_DIRS})
+target_include_directories(mpi SYSTEM INTERFACE
+  ${MPI_C_INCLUDE_PATH} ${MPI_C_INCLUDE_DIRS})
 
-target_link_libraries(mpi INTERFACE ${MPI_C_LIBRARIES} ${MPI_CXX_LIBRARIES})
+target_link_libraries(mpi INTERFACE ${MPI_C_LIBRARIES})
 
 install(TARGETS mpi EXPORT mpi)
 install(EXPORT mpi DESTINATION lib/cmake EXPORT_LINK_INTERFACE_LIBRARIES)
