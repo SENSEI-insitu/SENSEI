a45ab405539cbb5d1b2935b27d5dab005468f146
srizzi <srizzi@anl.gov>
2018-09-17 17:25:27 -0500

    renamed catalyst scripts. Added gaussian points. Cleaned up lammps input data

--- miniapps/lammps/test/gaussianpoints.py
+++ miniapps/lammps/test/gaussianpoints.py
@@ -34,40 +34,30 @@ def CreateCoProcessor():
 
       # Create a new 'Render View'
       renderView1 = CreateView('RenderView')
-      renderView1.ViewSize = [701, 529]
-      renderView1.AnnotationColor = [0.0, 0.0, 0.0]
+      renderView1.ViewSize = [1146, 780]
       renderView1.AxesGrid = 'GridAxes3DActor'
-      renderView1.OrientationAxesLabelColor = [0.0, 0.0, 0.0]
-      renderView1.OrientationAxesOutlineColor = [0.0, 0.0, 0.0]
-      renderView1.CenterOfRotation = [0.0004100799560546875, 0.0005474090576171875, -0.0021610260009765625]
+      renderView1.CenterOfRotation = [0.0004106329081778881, 0.0005481718430679905, -0.002161462923972124]
       renderView1.StereoType = 0
-      renderView1.CameraPosition = [171.6802520599626, 30.24467261981238, 158.44182161228704]
-      renderView1.CameraFocalPoint = [0.0004100799560546875, 0.0005474090576171875, -0.0021610260009765625]
-      renderView1.CameraViewUp = [0.033652204088957506, 0.974362722136786, -0.22245182595372806]
-      renderView1.CameraParallelScale = 60.96997278257824
-      renderView1.Background = [1.0, 1.0, 1.0]
+      renderView1.CameraPosition = [173.78645132236142, 89.87772075075492, 122.10831680362952]
+      renderView1.CameraFocalPoint = [3.4036604335758955, -5.084387807974572, -0.7912525646934538]
+      renderView1.CameraViewUp = [-0.29291118204268707, 0.9090598050915548, -0.2963331068261869]
+      renderView1.CameraParallelScale = 59.67022518864623
+      renderView1.Background = [0.32, 0.34, 0.43]
       renderView1.OSPRayMaterialLibrary = materialLibrary1
 
       # init the 'GridAxes3DActor' selected for 'AxesGrid'
-      renderView1.AxesGrid.XTitleColor = [0.0, 0.0, 0.0]
       renderView1.AxesGrid.XTitleFontFile = ''
-      renderView1.AxesGrid.YTitleColor = [0.0, 0.0, 0.0]
       renderView1.AxesGrid.YTitleFontFile = ''
-      renderView1.AxesGrid.ZTitleColor = [0.0, 0.0, 0.0]
       renderView1.AxesGrid.ZTitleFontFile = ''
-      renderView1.AxesGrid.GridColor = [0.0, 0.0, 0.0]
-      renderView1.AxesGrid.XLabelColor = [0.0, 0.0, 0.0]
       renderView1.AxesGrid.XLabelFontFile = ''
-      renderView1.AxesGrid.YLabelColor = [0.0, 0.0, 0.0]
       renderView1.AxesGrid.YLabelFontFile = ''
-      renderView1.AxesGrid.ZLabelColor = [0.0, 0.0, 0.0]
       renderView1.AxesGrid.ZLabelFontFile = ''
 
       # register the view with coprocessor
       # and provide it with information such as the filename to use,
       # how frequently to write the images, etc.
       coprocessor.RegisterView(renderView1,
-          filename='image_%t.png', freq=1, fittoscreen=0, magnification=1, width=701, height=529, cinema={})
+          filename='image_%t.png', freq=1, fittoscreen=0, magnification=1, width=1146, height=780, cinema={})
       renderView1.ViewTime = datadescription.GetTime()
 
       # ----------------------------------------------------------------
@@ -81,118 +71,89 @@ def CreateCoProcessor():
 
       # create a new 'XML MultiBlock Data Reader'
       # create a producer from a simulation input
-      test_multiblockvtm = coprocessor.CreateProducer(datadescription, 'atoms')
-
-      # create a new 'Glyph'
-      glyph1 = Glyph(Input=test_multiblockvtm,
-          GlyphType='Sphere')
-      glyph1.Scalars = ['POINTS', 'type']
-      glyph1.Vectors = ['POINTS', 'None']
-      glyph1.ScaleFactor = 7.6998101099470695
-      glyph1.GlyphMode = 'All Points'
-      glyph1.GlyphTransform = 'Transform2'
-
-      # init the 'Sphere' selected for 'GlyphType'
-      glyph1.GlyphType.Radius = 0.1
+      multiblockvtm = coprocessor.CreateProducer(datadescription, 'atoms')
 
       # ----------------------------------------------------------------
       # setup the visualization in view 'renderView1'
       # ----------------------------------------------------------------
 
-      # show data from glyph1
-      glyph1Display = Show(glyph1, renderView1)
+      # show data from multiblockvtm
+      multiblockvtmDisplay = Show(multiblockvtm, renderView1)
 
-      # get color transfer function/color map for 'GlyphScale'
-      glyphScaleLUT = GetColorTransferFunction('GlyphScale')
-      glyphScaleLUT.RGBPoints = [1.0, 0.231373, 0.298039, 0.752941, 34.5, 0.865003, 0.865003, 0.865003, 68.0, 0.705882, 0.0156863, 0.14902]
-      glyphScaleLUT.ScalarRangeInitialized = 1.0
+      # get color transfer function/color map for 'type'
+      typeLUT = GetColorTransferFunction('type')
+      typeLUT.RGBPoints = [1.0, 0.231373, 0.298039, 0.752941, 34.5, 0.865003, 0.865003, 0.865003, 68.0, 0.705882, 0.0156863, 0.14902]
+      typeLUT.ScalarRangeInitialized = 1.0
 
       # trace defaults for the display properties.
-      glyph1Display.Representation = 'Surface'
-      glyph1Display.AmbientColor = [0.0, 0.0, 0.0]
-      glyph1Display.ColorArrayName = ['POINTS', 'GlyphScale']
-      glyph1Display.LookupTable = glyphScaleLUT
-      glyph1Display.OSPRayScaleArray = 'GlyphScale'
-      glyph1Display.OSPRayScaleFunction = 'PiecewiseFunction'
-      glyph1Display.SelectOrientationVectors = 'GlyphScale'
-      glyph1Display.ScaleFactor = 7.714823532104493
-      glyph1Display.SelectScaleArray = 'GlyphScale'
-      glyph1Display.GlyphType = 'Arrow'
-      glyph1Display.GlyphTableIndexArray = 'GlyphScale'
-      glyph1Display.GaussianRadius = 0.3857411766052246
-      glyph1Display.SetScaleArray = ['POINTS', 'GlyphScale']
-      glyph1Display.ScaleTransferFunction = 'PiecewiseFunction'
-      glyph1Display.OpacityArray = ['POINTS', 'GlyphScale']
-      glyph1Display.OpacityTransferFunction = 'PiecewiseFunction'
-      glyph1Display.DataAxesGrid = 'GridAxesRepresentation'
-      glyph1Display.SelectionCellLabelFontFile = ''
-      glyph1Display.SelectionPointLabelFontFile = ''
-      glyph1Display.PolarAxes = 'PolarAxesRepresentation'
+      multiblockvtmDisplay.Representation = 'Point Gaussian'
+      multiblockvtmDisplay.ColorArrayName = ['POINTS', 'type']
+      multiblockvtmDisplay.LookupTable = typeLUT
+      multiblockvtmDisplay.OSPRayScaleArray = 'id'
+      multiblockvtmDisplay.OSPRayScaleFunction = 'PiecewiseFunction'
+      multiblockvtmDisplay.SelectOrientationVectors = 'None'
+      multiblockvtmDisplay.ScaleFactor = 7.6998101099470695
+      multiblockvtmDisplay.SelectScaleArray = 'None'
+      multiblockvtmDisplay.GlyphType = 'Arrow'
+      multiblockvtmDisplay.GlyphTableIndexArray = 'None'
+      multiblockvtmDisplay.GaussianRadius = 0.3849905054973535
+      multiblockvtmDisplay.SetScaleArray = ['POINTS', 'id']
+      multiblockvtmDisplay.ScaleTransferFunction = 'PiecewiseFunction'
+      multiblockvtmDisplay.OpacityArray = ['POINTS', 'id']
+      multiblockvtmDisplay.OpacityTransferFunction = 'PiecewiseFunction'
+      multiblockvtmDisplay.DataAxesGrid = 'GridAxesRepresentation'
+      multiblockvtmDisplay.SelectionCellLabelFontFile = ''
+      multiblockvtmDisplay.SelectionPointLabelFontFile = ''
+      multiblockvtmDisplay.PolarAxes = 'PolarAxesRepresentation'
 
       # init the 'PiecewiseFunction' selected for 'ScaleTransferFunction'
-      glyph1Display.ScaleTransferFunction.Points = [1.0, 0.0, 0.5, 0.0, 68.0, 1.0, 0.5, 0.0]
+      multiblockvtmDisplay.ScaleTransferFunction.Points = [1.0, 0.0, 0.5, 0.0, 32000.0, 1.0, 0.5, 0.0]
 
       # init the 'PiecewiseFunction' selected for 'OpacityTransferFunction'
-      glyph1Display.OpacityTransferFunction.Points = [1.0, 0.0, 0.5, 0.0, 68.0, 1.0, 0.5, 0.0]
+      multiblockvtmDisplay.OpacityTransferFunction.Points = [1.0, 0.0, 0.5, 0.0, 32000.0, 1.0, 0.5, 0.0]
 
       # init the 'GridAxesRepresentation' selected for 'DataAxesGrid'
-      glyph1Display.DataAxesGrid.XTitleColor = [0.0, 0.0, 0.0]
-      glyph1Display.DataAxesGrid.XTitleFontFile = ''
-      glyph1Display.DataAxesGrid.YTitleColor = [0.0, 0.0, 0.0]
-      glyph1Display.DataAxesGrid.YTitleFontFile = ''
-      glyph1Display.DataAxesGrid.ZTitleColor = [0.0, 0.0, 0.0]
-      glyph1Display.DataAxesGrid.ZTitleFontFile = ''
-      glyph1Display.DataAxesGrid.GridColor = [0.0, 0.0, 0.0]
-      glyph1Display.DataAxesGrid.XLabelColor = [0.0, 0.0, 0.0]
-      glyph1Display.DataAxesGrid.XLabelFontFile = ''
-      glyph1Display.DataAxesGrid.YLabelColor = [0.0, 0.0, 0.0]
-      glyph1Display.DataAxesGrid.YLabelFontFile = ''
-      glyph1Display.DataAxesGrid.ZLabelColor = [0.0, 0.0, 0.0]
-      glyph1Display.DataAxesGrid.ZLabelFontFile = ''
+      multiblockvtmDisplay.DataAxesGrid.XTitleFontFile = ''
+      multiblockvtmDisplay.DataAxesGrid.YTitleFontFile = ''
+      multiblockvtmDisplay.DataAxesGrid.ZTitleFontFile = ''
+      multiblockvtmDisplay.DataAxesGrid.XLabelFontFile = ''
+      multiblockvtmDisplay.DataAxesGrid.YLabelFontFile = ''
+      multiblockvtmDisplay.DataAxesGrid.ZLabelFontFile = ''
 
       # init the 'PolarAxesRepresentation' selected for 'PolarAxes'
-      glyph1Display.PolarAxes.PolarAxisTitleColor = [0.0, 0.0, 0.0]
-      glyph1Display.PolarAxes.PolarAxisTitleFontFile = ''
-      glyph1Display.PolarAxes.PolarAxisLabelColor = [0.0, 0.0, 0.0]
-      glyph1Display.PolarAxes.PolarAxisLabelFontFile = ''
-      glyph1Display.PolarAxes.LastRadialAxisTextColor = [0.0, 0.0, 0.0]
-      glyph1Display.PolarAxes.LastRadialAxisTextFontFile = ''
-      glyph1Display.PolarAxes.SecondaryRadialAxesTextColor = [0.0, 0.0, 0.0]
-      glyph1Display.PolarAxes.SecondaryRadialAxesTextFontFile = ''
+      multiblockvtmDisplay.PolarAxes.PolarAxisTitleFontFile = ''
+      multiblockvtmDisplay.PolarAxes.PolarAxisLabelFontFile = ''
+      multiblockvtmDisplay.PolarAxes.LastRadialAxisTextFontFile = ''
+      multiblockvtmDisplay.PolarAxes.SecondaryRadialAxesTextFontFile = ''
 
       # setup the color legend parameters for each legend in this view
 
-      # get color legend/bar for glyphScaleLUT in view renderView1
-      glyphScaleLUTColorBar = GetScalarBar(glyphScaleLUT, renderView1)
-      glyphScaleLUTColorBar.WindowLocation = 'AnyLocation'
-      glyphScaleLUTColorBar.Position = [0.8102710413694723, 0.5595463137996218]
-      glyphScaleLUTColorBar.Title = 'GlyphScale'
-      glyphScaleLUTColorBar.ComponentTitle = ''
-      glyphScaleLUTColorBar.TitleColor = [0.0, 0.0, 0.0]
-      glyphScaleLUTColorBar.TitleFontFile = ''
-      glyphScaleLUTColorBar.LabelColor = [0.0, 0.0, 0.0]
-      glyphScaleLUTColorBar.LabelFontFile = ''
-      glyphScaleLUTColorBar.ScalarBarLength = 0.32999999999999974
+      # get color legend/bar for typeLUT in view renderView1
+      typeLUTColorBar = GetScalarBar(typeLUT, renderView1)
+      typeLUTColorBar.Title = 'type'
+      typeLUTColorBar.ComponentTitle = ''
+      typeLUTColorBar.TitleFontFile = ''
+      typeLUTColorBar.LabelFontFile = ''
 
       # set color bar visibility
-      glyphScaleLUTColorBar.Visibility = 1
+      typeLUTColorBar.Visibility = 1
 
       # show color legend
-      glyph1Display.SetScalarBarVisibility(renderView1, True)
+      multiblockvtmDisplay.SetScalarBarVisibility(renderView1, True)
 
       # ----------------------------------------------------------------
       # setup color maps and opacity mapes used in the visualization
       # note: the Get..() functions create a new object, if needed
       # ----------------------------------------------------------------
 
-      # get opacity transfer function/opacity map for 'GlyphScale'
-      glyphScalePWF = GetOpacityTransferFunction('GlyphScale')
-      glyphScalePWF.Points = [1.0, 0.0, 0.5, 0.0, 68.0, 1.0, 0.5, 0.0]
-      glyphScalePWF.ScalarRangeInitialized = 1
+      # get opacity transfer function/opacity map for 'type'
+      typePWF = GetOpacityTransferFunction('type')
+      typePWF.Points = [1.0, 0.0, 0.5, 0.0, 68.0, 1.0, 0.5, 0.0]
+      typePWF.ScalarRangeInitialized = 1
 
       # ----------------------------------------------------------------
       # finally, restore active source
-      SetActiveSource(glyph1)
+      SetActiveSource(multiblockvtm)
       # ----------------------------------------------------------------
     return Pipeline()
 
@@ -202,7 +163,7 @@ def CreateCoProcessor():
 
   coprocessor = CoProcessor()
   # these are the frequencies at which the coprocessor updates.
-  freqs = {'atoms': [1, 1]}
+  freqs = {'atoms': [1]}
   coprocessor.SetUpdateFrequencies(freqs)
   return coprocessor
 
