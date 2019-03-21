d6cb2e9ddc0e822beda80d3516ee9d12a00daecd
guj <jgu@lbl.gov>
2019-03-21 13:00:51 -0700

    sensei coding style enforced

--- sensei/HDF5DataAdaptor.cxx
+++ sensei/HDF5DataAdaptor.cxx
@@ -18,23 +18,32 @@
 #include <pugixml.hpp>
 #include <sstream>
 
-namespace sensei {
+namespace sensei
+{
 
 //----------------------------------------------------------------------------
 senseiNewMacro(HDF5DataAdaptor);
 
 //----------------------------------------------------------------------------
-HDF5DataAdaptor::HDF5DataAdaptor() : m_HDF5Reader(nullptr) {}
+HDF5DataAdaptor::HDF5DataAdaptor()
+  : m_HDF5Reader(nullptr)
+{
+}
 
 //----------------------------------------------------------------------------
-HDF5DataAdaptor::~HDF5DataAdaptor() { delete m_HDF5Reader; }
+HDF5DataAdaptor::~HDF5DataAdaptor()
+{
+  delete m_HDF5Reader;
+}
 
 //----------------------------------------------------------------------------
-void HDF5DataAdaptor::SetStreamName(const std::string &name) {
+void HDF5DataAdaptor::SetStreamName(const std::string& name)
+{
   m_StreamName = name;
 }
 
-int HDF5DataAdaptor::Initialize(pugi::xml_node &node) {
+int HDF5DataAdaptor::Initialize(pugi::xml_node& node)
+{
   timer::MarkEvent mark("HDF5DataAdaptor::Initialize");
 
   this->InTransitDataAdaptor::Initialize(node);
@@ -45,40 +54,46 @@ int HDF5DataAdaptor::Initialize(pugi::xml_node &node) {
   if (filename)
     SetStreamName(filename.value());
 
-  if (methodAttr) {
-    std::string method = methodAttr.value();
+  if (methodAttr)
+    {
+      std::string method = methodAttr.value();
 
-    if (method.size() > 0) {
-      bool doStreaming = ('s' == method[0]);
-      bool doCollectiveTxf = ((method.size() > 1) && ('c' == method[1]));
+      if (method.size() > 0)
+        {
+          bool doStreaming = ('s' == method[0]);
+          bool doCollectiveTxf = ((method.size() > 1) && ('c' == method[1]));
 
-      SetStreaming(doStreaming);
-      SetCollective(doCollectiveTxf);
+          SetStreaming(doStreaming);
+          SetCollective(doCollectiveTxf);
+        }
     }
-  }
 
   return 0;
 }
 
 //----------------------------------------------------------------------------
 // int HDF5DataAdaptor::Open(const std::string& fileName)
-int HDF5DataAdaptor::OpenStream() {
+int HDF5DataAdaptor::OpenStream()
+{
   timer::MarkEvent mark("HDF5DataAdaptor::OpenStream");
 
-  if (m_StreamName.size() == 0) {
-    SENSEI_ERROR("Failed to specify stream name:\"");
-    return -1;
-  }
+  if (m_StreamName.size() == 0)
+    {
+      SENSEI_ERROR("Failed to specify stream name:\"");
+      return -1;
+    }
 
-  if (this->m_HDF5Reader == NULL) {
-    this->m_HDF5Reader =
+  if (this->m_HDF5Reader == NULL)
+    {
+      this->m_HDF5Reader =
         new senseiHDF5::ReadStream(this->GetCommunicator(), m_Streaming);
-  }
+    }
 
-  if (!this->m_HDF5Reader->Init(m_StreamName)) {
-    SENSEI_ERROR("Failed to open \"" << m_StreamName << "\"");
-    return -1;
-  }
+  if (!this->m_HDF5Reader->Init(m_StreamName))
+    {
+      SENSEI_ERROR("Failed to open \"" << m_StreamName << "\"");
+      return -1;
+    }
 
   // initialize the time step
   if (this->UpdateTimeStep())
@@ -87,7 +102,8 @@ int HDF5DataAdaptor::OpenStream() {
   return 0;
 }
 
-int HDF5DataAdaptor::StreamGood() {
+int HDF5DataAdaptor::StreamGood()
+{
   if (this->m_HDF5Reader == nullptr)
     return -1;
 
@@ -102,45 +118,51 @@ int HDF5DataAdaptor::StreamGood() {
 
 //----------------------------------------------------------------------------
 // int HDF5DataAdaptor::Close()
-int HDF5DataAdaptor::CloseStream() {
+int HDF5DataAdaptor::CloseStream()
+{
   timer::MarkEvent mark("HDF5DataAdaptor::Close");
   int m_Rank;
   MPI_Comm_rank(GetCommunicator(), &m_Rank);
 
-  if (this->m_HDF5Reader != nullptr) {
-    delete this->m_HDF5Reader;
-    this->m_HDF5Reader = nullptr;
-  }
+  if (this->m_HDF5Reader != nullptr)
+    {
+      delete this->m_HDF5Reader;
+      this->m_HDF5Reader = nullptr;
+    }
 
   return 0;
 }
 
 //----------------------------------------------------------------------------
-int HDF5DataAdaptor::Finalize() {
+int HDF5DataAdaptor::Finalize()
+{
   timer::MarkEvent mark("HDF5DataAdaptor::Finalize");
   return 0;
 }
 
 //----------------------------------------------------------------------------
 // int HDF5DataAdaptor::Advance()
-int HDF5DataAdaptor::AdvanceStream() {
+int HDF5DataAdaptor::AdvanceStream()
+{
   timer::MarkEvent mark("HDF5DataAdaptor::Advance");
 
   return this->UpdateTimeStep();
 }
 
 //----------------------------------------------------------------------------
-int HDF5DataAdaptor::UpdateTimeStep() {
+int HDF5DataAdaptor::UpdateTimeStep()
+{
   timer::MarkEvent mark("HDF5DataAdaptor::UpdateTimeStep");
 
   // update data object time and time step
   unsigned long timeStep = 0;
   double time = 0.0;
 
-  if (!this->m_HDF5Reader->AdvanceTimeStep(timeStep, time)) {
-    // SENSEI_ERROR("Failed to update time step");
-    return -1;
-  }
+  if (!this->m_HDF5Reader->AdvanceTimeStep(timeStep, time))
+    {
+      // SENSEI_ERROR("Failed to update time step");
+      return -1;
+    }
 
   this->SetDataTimeStep(timeStep);
   this->SetDataTime(time);
@@ -148,130 +170,152 @@ int HDF5DataAdaptor::UpdateTimeStep() {
   // read metadata
 
   unsigned int nMeshes = 0;
-  if (!this->m_HDF5Reader->ReadMetadata(nMeshes)) {
-    SENSEI_ERROR("Failed to read metadata at timestep: " << timeStep);
-    return -1;
-  }
+  if (!this->m_HDF5Reader->ReadMetadata(nMeshes))
+    {
+      SENSEI_ERROR("Failed to read metadata at timestep: " << timeStep);
+      return -1;
+    }
 
-  if (nMeshes == 0) {
-    SENSEI_ERROR("No Mesh at this timestep found");
-    return -1;
-  }
+  if (nMeshes == 0)
+    {
+      SENSEI_ERROR("No Mesh at this timestep found");
+      return -1;
+    }
   return 0;
 }
 
 //----------------------------------------------------------------------------
 int HDF5DataAdaptor::GetSenderMeshMetadata(unsigned int id,
-                                           MeshMetadataPtr &metadata) {
+                                           MeshMetadataPtr& metadata)
+{
   if (this->m_HDF5Reader->ReadMeshMetaData(id, metadata))
     return 0;
-  else {
-    SENSEI_ERROR("Failed to get metadata for object " << id)
-    return -1;
-  }
+  else
+    {
+      SENSEI_ERROR("Failed to get metadata for object " << id)
+      return -1;
+    }
 }
 
 //----------------------------------------------------------------------------
-int HDF5DataAdaptor::GetNumberOfMeshes(unsigned int &numMeshes) {
-  if (this->m_HDF5Reader) {
-    numMeshes = this->m_HDF5Reader->GetNumberOfMeshes();
-    return 0;
-  }
+int HDF5DataAdaptor::GetNumberOfMeshes(unsigned int& numMeshes)
+{
+  if (this->m_HDF5Reader)
+    {
+      numMeshes = this->m_HDF5Reader->GetNumberOfMeshes();
+      return 0;
+    }
 
   return -1;
 }
 
 //----------------------------------------------------------------------------
-int HDF5DataAdaptor::GetMeshMetadata(unsigned int id,
-                                     MeshMetadataPtr &metadata) {
+int HDF5DataAdaptor::GetMeshMetadata(unsigned int id, MeshMetadataPtr& metadata)
+{
   // check if some uber analysis told us how the data should land by
   // passing in reciever metadata
-  if (this->GetReceiverMeshMetadata(id, metadata)) {
-    // none set, we'll use the partitioner to figure it out
-    // first take a look at what's available
-    MeshMetadataPtr senderMd;
-    if (this->GetSenderMeshMetadata(id, senderMd)) {
-      SENSEI_ERROR("Failed to get sender metadata")
-      return -1;
-    }
+  if (this->GetReceiverMeshMetadata(id, metadata))
+    {
+      // none set, we'll use the partitioner to figure it out
+      // first take a look at what's available
+      MeshMetadataPtr senderMd;
+      if (this->GetSenderMeshMetadata(id, senderMd))
+        {
+          SENSEI_ERROR("Failed to get sender metadata")
+          return -1;
+        }
 
-    // get the partitioner, default to the block based layout
-    Partitioner *part = this->GetPartitioner();
-    if (!part) {
-      part = new BlockPartitioner();
-    }
+      // get the partitioner, default to the block based layout
+      Partitioner* part = this->GetPartitioner();
+      if (!part)
+        {
+          part = new BlockPartitioner();
+        }
 
-    MeshMetadataPtr recverMd;
-    if (part->GetPartition(this->GetCommunicator(), senderMd, recverMd)) {
-      SENSEI_ERROR("Failed to determine a suitable layout to receive the data");
-      this->CloseStream();
-    }
+      MeshMetadataPtr recverMd;
+      if (part->GetPartition(this->GetCommunicator(), senderMd, recverMd))
+        {
+          SENSEI_ERROR(
+            "Failed to determine a suitable layout to receive the data");
+          this->CloseStream();
+        }
 
-    metadata = recverMd;
-  }
+      metadata = recverMd;
+    }
 
   return 0;
 }
 
 //----------------------------------------------------------------------------
-int HDF5DataAdaptor::GetMesh(const std::string &meshName, bool structureOnly,
-                             vtkDataObject *&mesh) {
+int HDF5DataAdaptor::GetMesh(const std::string& meshName,
+                             bool structureOnly,
+                             vtkDataObject*& mesh)
+{
   timer::MarkEvent mark("HDF5DataAdaptor::GetMesh");
 
   mesh = nullptr;
 
   // other wise we need to read the mesh at the current time step
-  if (!this->m_HDF5Reader->ReadMesh(meshName, mesh, structureOnly)) {
-    SENSEI_ERROR("Failed to read mesh \"" << meshName << "\"");
-    return -1;
-  }
+  if (!this->m_HDF5Reader->ReadMesh(meshName, mesh, structureOnly))
+    {
+      SENSEI_ERROR("Failed to read mesh \"" << meshName << "\"");
+      return -1;
+    }
 
   return 0;
 }
 
 //----------------------------------------------------------------------------
-int HDF5DataAdaptor::AddGhostNodesArray(vtkDataObject *mesh,
-                                        const std::string &meshName) {
+int HDF5DataAdaptor::AddGhostNodesArray(vtkDataObject* mesh,
+                                        const std::string& meshName)
+{
   return AddArray(mesh, meshName, vtkDataObject::POINT, "vtkGhostType");
 }
 
 //----------------------------------------------------------------------------
-int HDF5DataAdaptor::AddGhostCellsArray(vtkDataObject *mesh,
-                                        const std::string &meshName) {
+int HDF5DataAdaptor::AddGhostCellsArray(vtkDataObject* mesh,
+                                        const std::string& meshName)
+{
   return AddArray(mesh, meshName, vtkDataObject::CELL, "vtkGhostType");
 }
 
 //----------------------------------------------------------------------------
-int HDF5DataAdaptor::AddArray(vtkDataObject *mesh, const std::string &meshName,
-                              int association, const std::string &arrayName) {
+int HDF5DataAdaptor::AddArray(vtkDataObject* mesh,
+                              const std::string& meshName,
+                              int association,
+                              const std::string& arrayName)
+{
   timer::MarkEvent mark("HDF5DataAdaptor::AddArray");
 
   // the mesh should never be null. there must have been an error
   // upstream.
-  if (!mesh) {
-    SENSEI_ERROR("Invalid mesh object");
-    return -1;
-  }
+  if (!mesh)
+    {
+      SENSEI_ERROR("Invalid mesh object");
+      return -1;
+    }
 
-  if (!this->m_HDF5Reader->ReadInArray(meshName, association, arrayName,
-                                       mesh)) {
-    SENSEI_ERROR("Failed to read " << VTKUtils::GetAttributesName(association)
-                                   << " data array \"" << arrayName
-                                   << "\" from mesh \"" << meshName << "\"");
-    return -1;
-  }
+  if (!this->m_HDF5Reader->ReadInArray(meshName, association, arrayName, mesh))
+    {
+      SENSEI_ERROR("Failed to read " << VTKUtils::GetAttributesName(association)
+                                     << " data array \"" << arrayName
+                                     << "\" from mesh \"" << meshName << "\"");
+      return -1;
+    }
 
   return 0;
 }
 
 //----------------------------------------------------------------------------
-int HDF5DataAdaptor::ReleaseData() {
+int HDF5DataAdaptor::ReleaseData()
+{
   timer::MarkEvent mark("HDF5DataAdaptor::ReleaseData");
   return 0;
 }
 
 //----------------------------------------------------------------------------
-void HDF5DataAdaptor::PrintSelf(ostream &os, vtkIndent indent) {
+void HDF5DataAdaptor::PrintSelf(ostream& os, vtkIndent indent)
+{
   this->sensei::DataAdaptor::PrintSelf(os, indent);
 }
 
