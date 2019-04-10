ce4d402dcc8fb0086a53f3a126d47077bce09c06
Burlen Loring <bloring@lbl.gov>
2019-04-09 17:16:05 -0700

    make the partitioners ref counted pointers
    this change is motivated by the difficulty dealing with unique_ptr
    in Python. a silver lining is that now the partitioners follow the
    same pattern as adaptors and metadata objects, which are all ref
    counted.

--- sensei/BlockPartitioner.h
+++ sensei/BlockPartitioner.h
@@ -6,17 +6,27 @@
 namespace sensei
 {
 
+class BlockPartitioner;
+using BlockPartitionerPtr = std::shared_ptr<BlockPartitioner>;
+
 /// @class BlockPartitioner
 /// The block partitioning mode will distribute blocks to a rank such that
 /// consecutive blocks share a rank.
 class BlockPartitioner : public sensei::Partitioner
 {
 public:
+  static BlockPartitionerPtr New()
+  { return BlockPartitionerPtr(new BlockPartitioner); }
+
   // given an existing partitioning of data passed in the first MeshMetadata
   // argument,return a new partittioning in the second MeshMetadata argument.
   // distributes blocks to a rank such that consecutive blocks share a rank.
   int GetPartition(MPI_Comm comm, const MeshMetadataPtr &in,
     MeshMetadataPtr &out) override;
+
+protected:
+  BlockPartitioner() = default;
+  BlockPartitioner(const BlockPartitioner &) = default;
 };
 
 }
--- sensei/CyclicPartitioner.h
+++ sensei/CyclicPartitioner.h
@@ -6,6 +6,9 @@
 namespace sensei
 {
 
+class CyclicPartitioner;
+using CyclicPartitionerPtr = std::shared_ptr<CyclicPartitioner>;
+
 /// @class CyclicPartitioner
 /// The cyclic distribution method will distribute blocks to a rank
 /// such that consecutive blocks are distributed over consecutive
@@ -13,11 +16,18 @@ namespace sensei
 class CyclicPartitioner : public sensei::Partitioner
 {
 public:
+  static CyclicPartitionerPtr New()
+  { return CyclicPartitionerPtr(new CyclicPartitioner); }
+
   // given an existing partitioning of data passed in the first MeshMetadata
   // argument,return a new partittioning in the second MeshMetadata argument.
   // blocks are distributed in a round-robin fashion.
   int GetPartition(MPI_Comm comm, const MeshMetadataPtr &in,
      MeshMetadataPtr &out) override;
+
+protected:
+  CyclicPartitioner() = default;
+  CyclicPartitioner(const CyclicPartitioner &) = default;
 };
 
 }
