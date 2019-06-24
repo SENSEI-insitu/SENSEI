ace108d46001ed84bea5b279efbb1f209a6f5ff1
Junmin Gu <jgu@lbl.gov>
2019-06-24 13:39:48 -0700

    enforcd hdf5 version to be parallel

--- CMake/hdf5.cmake
+++ CMake/hdf5.cmake
@@ -4,15 +4,18 @@ elseif(ENABLE_HDF5)
   find_package(HDF5 REQUIRED COMPONENTS C)
 endif()
 
-message(STATUS "HDF5 found? ${HDF5_FOUND} ${HDF5_INCLUDE_DIRS} ${HDF5_INCLUDE_DIR}")
+message(STATUS "check: HDF5 found? ${HDF5_FOUND} ${HDF5_INCLUDE_DIRS} ${HDF5_INCLUDE_DIR}")
+message(STATUS "check: HDF5 is parallel? ${HDF5_IS_PARALLEL}")
 
-if(HDF5_FOUND)
+if(HDF5_FOUND AND HDF5_IS_PARALLEL)
   add_library(sHDF5 INTERFACE)
   target_link_libraries(sHDF5 INTERFACE ${HDF5_LIBRARIES})
   target_include_directories(sHDF5 SYSTEM INTERFACE ${HDF5_INCLUDE_DIRS})
 
   install(TARGETS sHDF5 EXPORT sHDF5)
   install(EXPORT sHDF5 DESTINATION lib/cmake EXPORT_LINK_INTERFACE_LIBRARIES)
+ else()
+          message(SEND_ERROR "Failed to locate parallel hdf5 installation")	
 endif()
 
 
