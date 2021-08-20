7ff877a43d0fa58f87cabdfacafddbc0b3f48ff2
Ryan Krattiger <ryan.krattiger@kitware.com>
2021-08-20 15:51:52 -0500

    Updates to which and how CXX flags are set
    Use `add_compiler_options` instead of modifying the CMAKE_CXX_FLAGS directly
    Use CMAKE_<OPTION> to better support HPC compilers
    Remove micro architecture flags that are not supported by target system compilers (Fujitsu, Spock/Frontier, etc.)

--- CMake/build.cmake
+++ CMake/build.cmake
@@ -1,7 +1,8 @@
-option(BUILD_SHARED_LIBS OFF "Build shared libraries by default")
-option(BUILD_STATIC_EXECS  OFF "Link executables statically")
+option(BUILD_SHARED_LIBS ON "Build shared libraries (default: ON)")
+option(BUILD_STATIC_EXECS  OFF "Link executables statically.")
+
 if (BUILD_STATIC_EXECS)
-  set(BUILD_SHARED_LIBS OFF FORCE)
+  set(BUILD_SHARED_LIBS OFF)
   set(CMAKE_FIND_LIBRARY_SUFFIXES ${CMAKE_STATIC_LIBRARY_SUFFIX})
   set(LINK_SEARCH_START_STATIC TRUE)
   set(LINK_SEARCH_END_STATIC TRUE)
@@ -13,32 +14,26 @@ if(NOT CMAKE_BUILD_TYPE)
   set_property(CACHE CMAKE_BUILD_TYPE PROPERTY STRINGS "Debug" "Release" "MinSizeRel" "RelWithDebInfo")
 endif()
 
-if (NOT MSVC)
-  if (NOT CMAKE_CXX_FLAGS)
-  set(tmp "-fPIC -std=c++11 -Wall -Wextra")
-  if (BUILD_STATIC_EXECS)
-    set(tmp "${tmp} -static -static-libgcc -static-libstdc++ -pthread -Wl,-Bstatic")
-  endif()
-  if ((APPLE) AND ("${CMAKE_CXX_COMPILER_ID}" MATCHES "Clang"))
-    set(tmp "${tmp} -stdlib=libc++")
-  endif()
-  if ("${CMAKE_BUILD_TYPE}" MATCHES "Release")
-    set(tmp "${tmp} -O3 -march=native -mtune=native")
-  endif()
-  set(CMAKE_CXX_FLAGS "${tmp}"
-    CACHE STRING "SENSEI build defaults"
-    FORCE)
-  endif()
-endif()
+if (BUILD_STATIC_EXECS)
+  string(APPEND CMAKE_CXX_FLAGS "-static -static-libgcc -static-libstdc++ -pthread -Wl,-Bstatic")
+endif ()
 
 include_directories(${CMAKE_SOURCE_DIR})
 include_directories(${CMAKE_BINARY_DIR})
 
+message(STATUS "CMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE}")
+message(STATUS "BUILD_SHARED_LIBS=${BUILD_SHARED_LIBS}")
+message(STATUS "BUILD_STATIC_EXECS=${BUILD_STATIC_EXECS}")
+
+add_compile_options(
+   "$<$<NOT:$<COMPILE_LANGUAGE:CUDA>>:-Wall;-Wextra>"
+   # Need to explicitly set optimization level to -O3 because some systems default to -O2
+   "$<$<CONFIG:Release>:-O3>"
+   "$<$<CXX_COMPILER_ID:AppleClang>:-stdlib=libc++>")
+
 set(CMAKE_ARCHIVE_OUTPUT_DIRECTORY ${CMAKE_BINARY_DIR}/lib)
 set(CMAKE_LIBRARY_OUTPUT_DIRECTORY ${CMAKE_BINARY_DIR}/lib)
 set(CMAKE_RUNTIME_OUTPUT_DIRECTORY ${CMAKE_BINARY_DIR}/bin)
-
-message(STATUS "CMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE}")
-message(STATUS "CMAKE_CXX_FLAGS=${CMAKE_CXX_FLAGS}")
-message(STATUS "BUILD_SHARED_LIBS=${BUILD_SHARED_LIBS}")
-message(STATUS "BUILD_STATIC_EXECS=${BUILD_STATIC_EXECS}")
+set(CMAKE_CXX_STANDARD 11)
+set(CMAKE_CXX_EXTENSIONS OFF)
+set(CMAKE_POSITION_INDEPENDENT_CODE ON)
