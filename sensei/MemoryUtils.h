87c9b0c9efed3410b44073affb721df8915534fd
Burlen Loring <bloring@lbl.gov>
2021-09-22 10:49:44 -0700

    add MemoryUtils for dealing with memory in heterogeneous environments
    * runtime tests for if the data is accessible on the GPU or CPU
    * helpers to move the data seamlessly, returning shared_ptr so that
      memory is always cleaned up
    * shared_ptr deleter's that report what is being deleted.

---
+++ sensei/MemoryUtils.h
@@ -0,0 +1,72 @@
+#ifndef MemoryUtils_h
+#define MemoryUitls_h
+
+///@file
+
+#include <memory>
+
+//#define SENSEI_DEBUG 1
+
+/// SENSEI
+namespace sensei
+{
+/// Functions for dealing with memory access across heterogeneous architectures
+namespace MemoryUtils
+{
+
+/// return true if the pointer is accessible by code running on a CUDA GPU
+int CudaAccessible(const void *ptr);
+
+/// return true if the pointer is accessible by code running on the CPU
+int CpuAccessible(const void *ptr);
+
+/// callback that can free memory managed by CUDA
+void FreeCudaPtr(void *ptr);
+
+/// callback that can free memory managed by malloc
+void FreeCpuPtr(void *ptr);
+
+/// A callback that does not free the pointer
+void DontFreePtr(void *ptr);
+
+/** @name MakedCudaAccessible
+ * returns a pointer to data that is accessible from CUDA kernels. If the data
+ * is already accessible, this call is a noop. On the other hand if the data is
+ * not accessible it will be moved. The accessible data is returned as a shared
+ * pointer so that in the case data needed to  be moved, the buffers on the GPU
+ * will automatically be released.
+ */
+///@{
+std::shared_ptr<void> MakeCudaAccessible_(void *ptr, size_t nBytes);
+
+template <typename data_t>
+std::shared_ptr<data_t> MakeCudaAccessible(data_t *ptr, size_t nVals)
+{
+  return std::static_pointer_cast<data_t>(
+    sensei::MemoryUtils::MakeCudaAccessible_(
+      (void*)ptr, nVals*sizeof(data_t)));
+}
+///@}
+
+/** @name MakeCpuAccessible
+ * returns a pointer to data that is accessible from CPU codes. If the data is
+ * already accessible, this call is a noop. On the other hand if the data is
+ * not accessible it will be moved. The accessible data is returned as a shared
+ * pointer so that in the case data needed to  be moved, the buffers on the CPU
+ * will automatically be released.
+ */
+///@{
+std::shared_ptr<void> MakeCpuAccessible_(void *ptr, size_t nBytes);
+
+template <typename data_t>
+std::shared_ptr<data_t> MakeCpuAccessible(data_t *ptr, size_t nVals)
+{
+  return std::static_pointer_cast<data_t>(
+    sensei::MemoryUtils::MakeCpuAccessible_(
+      (void*)ptr, nVals*sizeof(data_t)));
+}
+///@}
+
+}
+}
+#endif
