8d26ad83bde2405f7cc9309009349b9841ba4955
Gordon A. Acocella <gaa@ilight.com>
2023-03-10 08:21:00 -0700

    Support Kombyne lite and Kombyne.

--- CMake/kombyne.cmake
+++ CMake/kombyne.cmake
@@ -1,14 +1,38 @@
 if(ENABLE_KOMBYNE)
+  MESSAGE("ENABLE_KOMBYNE: YES!")
   cmake_policy(SET CMP0074 NEW)
-  set(kombyne_ROOT "${KOMBYNE_DIR}")
-  # gaatmp: should this be KOMBYNE_ENABLE_CUDA?
   if(ENABLE_CUDA)
     enable_language(CUDA)
   endif()
-  find_package(kombyne)
-  add_library(sKombyne INTERFACE)
-  target_link_libraries(sKombyne INTERFACE kombyne)
-  install(TARGETS sKombyne EXPORT sKombyne)
-  install(EXPORT sKombyne DESTINATION ${sensei_CMAKE_INSTALL_CMAKEDIR}
-    EXPORT_LINK_INTERFACE_LIBRARIES)
+
+  if(DEFINED KOMBYNE_DIR)
+    set(kombyne_ROOT "${KOMBYNELITE_DIR}")
+    find_package(kombyne)
+
+    add_library(sKombyne INTERFACE)
+    target_link_libraries(sKombyne INTERFACE kombyne)
+    target_include_directories(sKombyne SYSTEM INTERFACE
+      "${KB_INSTALL_INCLUDEDIR}/kombyne")
+
+    install(TARGETS sKombyne EXPORT sKombyne)
+    install(EXPORT sKombyne DESTINATION ${sensei_CMAKE_INSTALL_CMAKEDIR}
+      EXPORT_LINK_INTERFACE_LIBRARIES)
+
+  elseif(DEFINED KOMBYNELITE_DIR)
+    set(kombynelite_ROOT "${KOMBYNELITE_DIR}")
+    find_package(kombynelite)
+
+    add_library(sKombyne INTERFACE)
+    target_link_libraries(sKombyne INTERFACE kombynelite)
+    target_include_directories(sKombyne SYSTEM INTERFACE
+      "${KB_INSTALL_INCLUDEDIR}/kombynelite")
+
+    install(TARGETS sKombyne EXPORT sKombyne)
+    install(EXPORT sKombyne DESTINATION ${sensei_CMAKE_INSTALL_CMAKEDIR}
+      EXPORT_LINK_INTERFACE_LIBRARIES)
+
+  else()
+    MESSAGE("got nothing")
+  endif()
+
 endif()
