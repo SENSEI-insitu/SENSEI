2c18de6ba5296c07e2adc6be66fffadc5e81f47f
Will Dunklin <will.dunklin@kitware.com>
2023-05-24 16:37:44 +0000

    Remove fedora test image/runner

--- .github/ci/configure_fedora35_ecp_catalyst.cmake
+++
@@ -1,8 +0,0 @@
-include(${CMAKE_CURRENT_LIST_DIR}/configure_adios2.cmake)
-include(${CMAKE_CURRENT_LIST_DIR}/configure_ascent.cmake)
-include(${CMAKE_CURRENT_LIST_DIR}/configure_catalyst.cmake)
-include(${CMAKE_CURRENT_LIST_DIR}/configure_hdf5.cmake)
-include(${CMAKE_CURRENT_LIST_DIR}/configure_python3.cmake)
-include(${CMAKE_CURRENT_LIST_DIR}/configure_vtkio.cmake)
-
-include(${CMAKE_CURRENT_LIST_DIR}/configure_fedora35.cmake)
--- .github/ci/docker/fedora35-ecp-catalyst/configure-env.sh
+++
@@ -1,9 +0,0 @@
-. ${SPACK_ROOT}/share/spack/setup-env.sh
-
-if [[ ! -z ${SENSEI_ENV} ]]; then
-  module use ${SPACK_ROOT}/share/spack/lmod/linux-fedora35-x86_64/Core
-  module load openmpi
-  . ${SPACK_ROOT}/var/spack/environments/${SENSEI_ENV}/loads
-else
-  echo "No environment configured."
-fi
